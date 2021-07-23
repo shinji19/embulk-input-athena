895f3d08399683feff949ab3735f5627e304676d
hi1280 <hi12801280@gmail.com>
2021-07-23 18:15:25 +0900

    Update Athena JDBC Driver 1.1.0 to 2.0.23

--- src/main/java/org/embulk/input/athena/AthenaInputPlugin.java
+++ src/main/java/org/embulk/input/athena/AthenaInputPlugin.java
@@ -259,7 +259,7 @@ public class AthenaInputPlugin implements InputPlugin
 
     protected Connection getAthenaConnection(PluginTask task) throws ClassNotFoundException, SQLException
     {
-        loadDriver("com.amazonaws.athena.jdbc.AthenaDriver", task.getDriverPath());
+        loadDriver("com.simba.athena.jdbc.Driver", task.getDriverPath());
         Properties properties = new Properties();
         properties.put("s3_staging_dir", task.getS3StagingDir());
         properties.put("user", task.getAccessKey());
